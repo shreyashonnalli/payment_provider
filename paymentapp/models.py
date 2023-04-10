f873d2a3a4a8405cea2d28ecbfdfca6f18655b0c
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-04-10 14:55:48 +0100

    db schema done and api keys added

--- paymentapp/models.py
+++ paymentapp/models.py
@@ -1,11 +1,46 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
 class Currency(models.Model):
     code = models.CharField(max_length=3, primary_key=True)
     name = models.CharField(max_length=30)
-    
+
+
 class Merchant(models.Model):
-    id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    API_KEY = models.CharField(max_length=20)
+    API_KEY = models.CharField(max_length=50)
+
+
+class Transaction(models.Model):
+    date = models.DateField()
+    status = models.CharField(max_length=15)
+    amount = models.IntegerField()
+    checkouts = models.ManyToManyField('Checkout')
+
+
+class Checkout(models.Model):
+    # FK
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    # FK
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    status = models.CharField(max_length=15)
+    date = models.DateField()
+    number = models.IntegerField(validators=[MinValueValidator(
+        1000000000000000), MaxValueValidator(9999999999999999)])
+    exp_month = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)])
+    exp_year = models.IntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(9999)])
+    name = models.CharField(max_length=20)
+    cvv = models.IntegerField(
+        validators=[MinValueValidator(100), MaxValueValidator(999)])
+    # FK
+    transactions = models.ManyToManyField(Transaction)
+    installments = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
+    type = models.IntegerField()
