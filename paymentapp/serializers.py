d7da9b5ca1f9b8df4d605afdc639b1b306546cc2
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-05-10 13:39:23 +0100

    changed represenations when returning

--- paymentapp/serializers.py
+++ paymentapp/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from paymentapp.models import Currency, Merchant, Transaction, Checkout
+from datetime import datetime
+from decimal import Decimal
 
 
 class MerchantSerializer(serializers.ModelSerializer):
@@ -15,21 +17,42 @@ class CurrencySerializer(serializers.ModelSerializer):
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField()
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+
     class Meta:
         model = Transaction
         exclude = ("checkouts",)
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["date"] = (representation["date"])[:-1]
+        representation["amount"] = Decimal(representation["amount"])
+        return representation
+
 
 class CheckoutSerializer(serializers.ModelSerializer):
     transactions = TransactionSerializer(many=True, read_only=True)
+    date = serializers.DateTimeField()
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Checkout
         fields = "__all__"
+
+    """
+    def get_iso_date(self, obj):
+        if isinstance(obj, datetime):
+            print(obj)
+            return obj.isoformat()
+        return obj
+    """
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation["transactions"] = TransactionSerializer(
             instance.transactions.all(), many=True
         ).data
+        representation["date"] = (representation["date"])[:-1]
+        representation["amount"] = Decimal(representation["amount"])
         return representation
